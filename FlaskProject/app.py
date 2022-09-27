aad325aac613e806224f4750cb897cf1d12ffda9
pharmlovex <pharmifedayoojo@gmail.com>
2022-09-27 02:25:17 +0100

    workinginprogress

---
+++ FlaskProject/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+memcache={}
+
+@app.route("/")
+def salute():
+    return "Hello Dayo! Great Job"
+
+@app.route("/about")
+def about():
+    return "A bio-datascientist"
+
+
+@app.route("/about/<name>")
+def name(name):
+    return f"Tell more about {name}" 
+
+@app.route("/cache", methods=["GET","POST"])
+def cache():
+    if request.method == "POST":
+        memcache.update(request.json)
+        return "Success"
+    else:
+        return jsonify(memcache)
+
